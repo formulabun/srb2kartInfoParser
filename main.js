0dfeeab5b603db0bb78e2de45f1e79523695794c
Fl_GUI <flor.guilini@hotmail.com>
2021-05-20 12:10:35 +0200

    parse server info

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-#!/usr/bin/env node
 const dgram = require('dgram');
 
-
 const parseServerInfo = (msg) => {
-  return {};
+  const res = {};
+  var offset = 0;
+  // header
+  const header = {};
+  header.checksum = msg.slice(offset, offset+4); offset += 4;
+  header.ack = msg.slice(offset, ++offset);
+  header.ackret = msg.slice(offset, ++offset);
+  header.packettype = msg.slice(offset, ++offset)
+  msg.slice(offset, ++offset);
+  res.header = header;
+
+  // important data
+  res._255 = msg.readInt8(offset++);
+  res.packetversion = msg.readInt8(offset++);
+  res.application = msg.toString('utf-8', offset, offset+16); offset += 16;
+  res.version = msg.readInt8(offset++);
+  res.subversion = msg.readInt8(offset++);
+  res.numberofplayers = msg.readInt8(offset++);
+  res.maxplayers = msg.readInt8(offset++);
+  res.gametype = msg.readInt8(offset++);
+  res.modifiedgame = msg.readInt8(offset++);
+  res.cheatsenabled = msg.readInt8(offset++);
+  res.kartvars = msg.readInt8(offset++);
+  res.fileneedednum = msg.readInt8(offset++);
+  res.time = msg.readInt32LE(offset); offset+= 4;
+  res.leveltime = msg.readInt32LE(offset); offset+= 4;
+  res.servername = msg.toString('utf-8', offset, offset+32); offset += 32;
+  res.mapname = msg.toString('utf-8', offset, offset+8); offset += 8;
+  res.maptitle = msg.toString('utf-8', offset, offset+33); offset += 33;
+  res.mapmd5 = msg.slice(offset, offset+16); offset += 16;
+  res.actnum = msg.readInt8(offset++);
+  res.iszon = msg.readInt8(offset++);
+  res.httpsource = msg.toString('utf-8', offset, offset+256); offset+=256;
+  res.fileneeded = msg.slice(offset, offset+915); offset+=915;
+
+  return res;
 }
 
 const parsePlayerInfo = (msg) => {
-  return {};
+  const res = {};
+  // TODO everything
+  return res;
 }
 
 const getSrb2Info = (address, port=5029, servercb, playercb) => {
@@ -41,4 +76,6 @@ const getSrb2Info = (address, port=5029, servercb, playercb) => {
   sock.connect(port, address);
 }
 
-getSrb2Info("formulabun.club", 5029, console.log, console.log);
+if(!module.parent)  {
+  getSrb2Info("formulabun.club", 5029, console.log, console.log);
+}
