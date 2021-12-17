3b105da14c5725bdbecc2ea704402494660b03ad
Fl0GUI <flor.guilini@hotmail.com>
2021-12-17 11:51:18 +0100

    real entry file

---
+++ main.js
@@ -0,0 +1,12 @@
+import getSrb2Info from './srb2kartserverinfo.js';
+import logger from './log.js';
+import { extractGraphics, extractSoc } from './pk3parse.js';
+import parseSocFile from './socparse.js';
+
+export {
+  getSrb2Info,
+  logger,
+  extractGraphics, 
+  extractSoc,
+  parseSocFile,
+}
